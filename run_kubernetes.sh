18a8722be459b16da4e01b384490ff7729019349
xia0m <alexma325@gmail.com>
2020-02-07 03:06:17 -0800

    finished kubernetes output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="xia0m/sklearn"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sklearn --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward sklearn-564bb77876-szr2t 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=xia0m/sklearn
+dockertag=latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag sklearn $dockerpath:$dockertag
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:$dockertag
